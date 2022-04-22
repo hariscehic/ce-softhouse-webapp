0d14f86af3144dd32678ef7912bbc47b98ab3d5a
Haris Cehic <haris.cehic@precisebiometrics.com>
2022-04-22 11:27:53 +0200

    fix functions

--- api/db/index.js
+++ api/db/index.js
@@ -1,34 +1,46 @@
-import { Schema, model, connect } from "mongoose";
+const mongoose = require("mongoose");
 
-let db=null;
+let db = null;
 
-const ContentSchema = new Schema(
+const ContentSchema = new mongoose.Schema(
   { type: String },
   { header: String },
   { body: String },
-  { timestamps: true },
+  { timestamps: true }
 );
 
-const ContentModel = model("Content", ContentSchema);
+const ContentModel = mongoose.model("Content", ContentSchema);
 
-export const init = async () => {
-  if(!db) {
-    db = await connect("mongodb://ce-softhouse-db:TKIalmA18B3DjVZis1y0ohczQWAHAQxgTBxlV31kKxp6N9n5SqDFyLfOM0gmxmnMCma8SVS8WRfgcoupy2kmig%3D%3D@ce-softhouse-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@ce-softhouse-db@",  { useNewUrlParser: true });
+const init = async () => {
+  if (!db) {
+    db = await mongoose.connect(
+      "mongodb://ce-softhouse-db:TKIalmA18B3DjVZis1y0ohczQWAHAQxgTBxlV31kKxp6N9n5SqDFyLfOM0gmxmnMCma8SVS8WRfgcoupy2kmig%3D%3D@ce-softhouse-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@ce-softhouse-db@",
+      { useNewUrlParser: true }
+    );
+    console.log(db);
   }
 };
-export const addContent = async (doc) => {
+const addContent = async (doc) => {
   const modelToInsert = new ContentModel();
   return await modelToInsert.save(doc);
 };
 
-export const getContentyById = async (id) => {
+const getContentyById = async (id) => {
   return await ContentModel.findById(id);
 };
 
-export const getContent = async (query = {}) => {
+const getContent = async (query = {}) => {
   return await ContentModel.find({});
 };
 
-export const deleteContent = async (id) => {
+const deleteContent = async (id) => {
   return await ContentModel.findByIdAndDelete(id);
+};
+
+module.exports = {
+  init,
+  addContent,
+  getContent,
+  getContentyById,
+  deleteContent,
 };
